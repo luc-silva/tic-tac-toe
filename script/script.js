518428c843d9d48ae754dae9ef6a21f2273973c1
Lucas Santos <lucas.silva1203@outlook.com>
2022-11-03 18:13:32 -0300

    Add turns
    Add function that changes the current player
    Also, modify function that add icon to the field

--- script/script.js
+++ script/script.js
@@ -1,37 +1,69 @@
-// const playerOne = (function(){
-//     const icn;
+const playerTwo = (function(){
+    //const getMarker = () =>{}
+    let marker = "o"
+    return {marker}
+})()
+const playerOne = (function(){
+    //const getMarker = () =>{}
+    let marker = "x"
+    return {marker}
+})()
 
+let game = (function(){
+    let currentPlayer = playerOne
+    let nextPlayer = playerTwo
 
-// }
+    const changeCurrentPlayer = function (){
+        [this.currentPlayer, this.nextPlayer] = [this.nextPlayer, this.currentPlayer] 
+    }
 
-// )()
+    const startGame = () => {
+        let rounds = document.querySelector("round-input")
+        _gameBoard.clearField()
+    }
 
-const _gameBoard = (function(){
+    return {currentPlayer, nextPlayer, startGame, changeCurrentPlayer }
+})()
+
+let _gameBoard = (function(){
     let field = document.querySelector("#field")
+    field.addEventListener("click", (e) =>{
+        if(e.target.textContent == ""){
+            e.target.textContent = game.currentPlayer.marker
+            game.changeCurrentPlayer()
+        }
+    })
 
+    let message = document.querySelector("#message")
+    let fieldValues = ["", "", "", 
+    "", "", "", 
+    "", "", "", ]
+    
     let clearField = function (){
         field.textContent = " "
-        addBlock()
-        getBlocks()
+        field.style.display = "grid"
+        
+        createBlock()
     }
     
-    const addBlock = function() {
-        for(let i = 0; i != 9; i++){
+    const createBlock = (function() {
+        for(let block of fieldValues){
             let lol = document.createElement("div")
             lol.classList.add("block")
+            lol.textContent = block
             field.append(lol) 
         }
-    }
+    })
     
-    const getBlocks = function(){
-        let blocks = document.querySelectorAll(".block")
-        blocks.forEach(block => {
-            block.addEventListener("click", () => {
-                block.textContent = "X" //currentPLayer.icn
-            })
-        })
-    }
+    let blocks = document.querySelectorAll(".block")
+ 
+    return {clearField}
+                
+    })()
 
-    return {clearField, addBlock}
-
+const _gamePanel = (function (){
+    let startBtn = document.querySelector("#start-btn")
+    startBtn.addEventListener("click", () => {
+        game.startGame()
+    })
 })()
